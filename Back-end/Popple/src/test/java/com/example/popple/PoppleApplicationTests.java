e102035636bb5c15722a98b6819a93131d66c945
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-10-10 19:04:39 +0900

    init : popple project

---
+++ Back-end/Popple/src/test/java/com/example/popple/PoppleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
