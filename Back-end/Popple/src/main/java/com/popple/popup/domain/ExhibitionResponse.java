e8f6a2faf65282998638f7f4c58df09ff6cfe712
whgms159 <whgms159@naver.com>
2024-10-15 17:13:22 +0900

    fix : authpackage

--- Back-end/Popple/src/main/java/com/popple/popup/domain/ExhibitionResponse.java
+++
@@ -1,5 +0,0 @@
-package com.popple.popup.domain;
-
-public class ExhibitionResponse {
-
-}
--- Back-end/Popple/src/main/java/com/popple/popup/repository/ExhibitionRespository.java
+++
@@ -1,7 +0,0 @@
-package com.popple.popup.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExhibitionRespository extends JpaRepository<PopUp, Long> {
-
-}
