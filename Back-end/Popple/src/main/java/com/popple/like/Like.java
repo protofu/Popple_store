08bbcec08d3fd6d9c1729172ba5c0eeec7f92772
whgms159 <whgms159@naver.com>
2024-10-15 18:32:53 +0900

    feat : like

---
+++ Back-end/Popple/src/main/java/com/popple/like/Like.java
@@ -0,0 +1,39 @@
+package com.popple.like;
+
+import com.popple.auth.entity.User;
+import com.popple.exhibition.entity.Exhibition;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Like {
+	//id
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false)
+	private Long id;
+	
+	@JoinColumn(name = "user_id", nullable = false)
+	@ManyToOne
+	private User user;
+	
+	@JoinColumn(name = "exhibition_id", nullable = false)
+	@OneToOne
+	private Exhibition exhibition;
+		
+}
