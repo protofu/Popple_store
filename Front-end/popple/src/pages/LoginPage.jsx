bbd92830209cc3103e887b86d7ae189f3b4afb60
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-10-16 17:03:28 +0900

    feat : border logo position

--- Front-end/popple/src/pages/LoginPage.jsx
+++ Front-end/popple/src/pages/LoginPage.jsx
@@ -2,18 +2,13 @@ import styles from "./styles/LoginPage.module.css";
 import kakao from "../assets/kakao_symbol.png";
 import google from "../assets/google_symbol.png";
 import company from "../assets/company.png";
-import Logo from "../assets/MainLogo.png";
 import CustomLoginButton from "../components/common/CustomLoginButton";
 
 export default function LoginPage() {
   return (
     <div className={styles.loginContainer}>
 
-      <img src={Logo} alt="로고이미지" className="
-        w-[180px]
-        bg-white
-        block absolute left-[calc(50%-90px)] top-[calc(7.5%-31px)]
-      "/>
+      
       <input type="text" />
       <input type="text" />
       <div className={styles.loginButton}>
--- Front-end/popple/src/pages/layouts/AuthLayout.jsx
+++ Front-end/popple/src/pages/layouts/AuthLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Logo from "../assets/MainLogo.png";
 
 export default function AuthLayout() {
   return (
@@ -11,6 +12,11 @@ export default function AuthLayout() {
       w-full p-4 mx-auto
       border border-[#8900E1]
     ">
+      <img src={Logo} alt="로고이미지" className="
+        w-[180px]
+        bg-white
+        block absolute left-[calc(50%-90px)] top-[calc(7.5%-31px)]
+      "/>
       <Outlet />
     </div>
   );
