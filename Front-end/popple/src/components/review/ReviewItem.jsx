a77c9e265552f8a0b0e3b746215c1233b3380c8d
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-10-24 18:13:04 +0900

    feat : detail review

--- Front-end/popple/src/components/review/ReviewItem.jsx
+++ Front-end/popple/src/components/review/ReviewItem.jsx
@@ -1,10 +1,15 @@
 import reviewImg from "../../assets/review-img.png";
 
 export default function ReviewItem({ review }) {
-  const date = new Date(review.createdAt);
-  // 연도, 월, 일, 시, 분 추출
-  const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+  const createdAt = review.createdAt; // createdAt 배열
+  
+  // 배열에서 각 요소를 추출
+  const [year, month, day, hour, minute] = createdAt;
+  const date = new Date(year, month - 1, day, hour, minute); // month는 0부터 시작하므로 -1
 
+  // 연도, 월, 일, 시, 분 형식
+  const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+  
   // 이미지 불러오기
   const imageUrl = `${import.meta.env.VITE_SERVER}/review_image/${review.image?.savedName}`;
   console.log(imageUrl);
