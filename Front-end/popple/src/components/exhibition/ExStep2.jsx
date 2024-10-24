6c6f6ebbef177c6dcfb8c08feb9075fb6608d17f
whgms159 <whgms159@naver.com>
2024-10-24 19:52:36 +0900

    feat : ExStep

--- Front-end/popple/src/components/exhibition/ExStep2.jsx
+++ Front-end/popple/src/components/exhibition/ExStep2.jsx
@@ -1,66 +1,59 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CompanyAuthAPI } from "../../api/services/CompanyAuth";
 
-const ExStep2 = ({ information, changeInformation }) => {
-  const inputStyle =
-    "w-full h-[50px] border border-[#ccc] rounded-[8px] focus:border-[#8900E1] focus:border-2 block focus:outline-none px-2";
-  const smallStyle =
-    "w-2/5 h-[50px] border border-[#ccc] rounded-[8px] focus:border-[#8900E1] focus:border-2 focus:outline-none px-2";
-  return (
-    <>
-      <div className="grid grid-cols-2 gap-x-20 gap-y-10">
-        <label className="text-sm">
-          주최/기획<span className="text-red-500">*</span>
-          <input
-            name="company.name"
-            className={inputStyle}
-            readOnly
-            value={information.company.name}
-            onChange={(e) => changeInformation(e)}
-          />
-        </label>
-        <label className="text-sm">
-          사업자 등록번호<span className="text-red-500">*</span>
-          <input
-            name="company.businessNumber"
-            className={inputStyle}
-            readOnly
-            value={information.company.businessNumber}
-            onChange={(e) => changeInformation(e)}
-          />
-        </label>
-        <label className="text-sm">
-          기업 전화번호<span className="text-red-500">*</span>
-          <input
-            name="company.contact"
-            className={inputStyle}
-            readOnly
-            value={information.company.contact}
-            onChange={(e) => changeInformation(e)}
-          />
-        </label>
-        <label className="text-sm">
-          기업 주소<span className="text-red-500">*</span>
-          <input
-            name="company.address"
-            className={inputStyle}
-            readOnly
-            value={information.company.address}
-            onChange={(e) => changeInformation(e)}
-          />
-        </label>
-        <label className="text-sm">
-          유효 기간/이용 조건
-          <input
-            name="company.condition"
-            className={inputStyle}
-            readOnly
-            value={information.company.condition}
-            onChange={(e) => changeInformation(e)}
-          />
-        </label>
-      </div>
-    </>
-  );
-};
+const ExStep2 = ({information, changeInformation}) => {
+    // input 태그 스타일 지정
+    const inputStyle = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg inline-block p-2.5 mb-10";
 
+    const [companyInfo, setCompanyInfo] = useState({});
+
+    // 로그인한 기업 회원으로부터 정보 가져오기
+    const getCompanyInfo = async(a) => {
+        try {
+            const res = await CompanyAuthAPI.get(a)
+            const data = res.data;
+            console.log(data)
+            
+        } catch (error) {
+            console.log(error)
+        }
+       
+    }
+    
+
+    useEffect(() => {
+        getCompanyInfo();       
+    }, []);
+
+    return (
+        <>
+            {
+                companyInfo &&
+                <div className="grid grid-cols-2 gap-x-10 w-full h-full">
+                    <div className="flex flex-col">
+                        <label>주최/기획</label>
+                        <input name="companyName" className={inputStyle} value={companyInfo.companyName} readOnly />
+                    </div>
+                    <div className="flex flex-col">
+                        <label>사업자 등록번호</label>
+                        <input name="businessNumber" className={inputStyle} value={companyInfo.businessNumber} readOnly />
+                    </div>
+                    <div className="flex flex-col">
+                        <label>기업 전화번호</label>
+                        <input name="contact" className={inputStyle} value={companyInfo.contact} readOnly/>
+                    </div>
+                    <div className="flex flex-col">
+                        <label>기업 주소</label>
+                        <input name="companyAddress" className={inputStyle} value={companyInfo.companyAddress} readOnly />
+                    </div>
+                    <div className="flex flex-col">
+                        <label>유효 기간/이용 조건</label>
+                        <input name="terms" className={inputStyle} value={information.terms} onChange={(e) => changeInformation(e)} />
+                    </div>
+                </div>
+            }
+        </>
+    );
+}
+ 
 export default ExStep2;
--- Front-end/popple/src/pages/ExhibitionRegistPage.jsx
+++ Front-end/popple/src/pages/ExhibitionRegistPage.jsx
@@ -2,58 +2,54 @@ import { useEffect, useState } from "react";
 import ExhibitionHeader from "../components/exhibition/ExhibitionHeader";
 import ExStep1 from "../components/exhibition/ExStep1";
 import ExStep2 from "../components/exhibition/ExStep2";
-import { useNavigate } from "react-router-dom";
 import EventRegister from "./EventRegister";
+import { ExhibitionAPI } from "../api/services/Exhibition";
+import { data } from "autoprefixer";
 
 export default function ExhibitionRegistPage() {
   const [step, setStep] = useState(1);
-
   const [information, setInformation] = useState({
-    title: "", // 팝업/전시명
-    subtitle: "", // 부제
+    exhibitionName: "", // 팝업/전시명
+    subTitle: "", // 부제
+    free: false, // 무료 여부
     fee: "", // 입장료
-    startDate: "", // 팝업 전시 기간 (시작일)
-    endDate: "", // 팝업 전시 기간 (종료일)
-    description: "", // 상세 설명
-    descriptionImg: "", // 상세 설명 포스터 이미지
-    roadAddr: "", // 도로명 주소
-    detailAddr: "", // 상세 주소
+    startAt: "", // 팝업 전시 기간 (시작일)
+    endAt: "", // 팝업 전시 기간 (종료일)
+    detailDescription: "", // 상세 설명
+    poster: "", // 상세 설명 포스터 이미지
+    address: "", // 도로명 주소
+    detailAddress: "", // 상세 주소
     homepageLink: "", // 홈페이지 링크
     instagramLink: "", // 인스타그램 링크
     notice: "", // 공지사항
-    images: [], // 이미지
+    image: [], // 이미지
+    terms: "", // 유효 기간/이용 조건
     openTime: {
-      mon: { open: "", close: "", holiday: false },
-      tue: { open: "", close: "", holiday: false },
-      wed: { open: "", close: "", holiday: false },
-      thu: { open: "", close: "", holiday: false },
-      fri: { open: "", close: "", holiday: false },
-      sat: { open: "", close: "", holiday: false },
-      sun: { open: "", close: "", holiday: false },
+      monday: { open: "", close: "", holiday: false },
+      tuesday: { open: "", close: "", holiday: false },
+      wednesday: { open: "", close: "", holiday: false },
+      thursday: { open: "", close: "", holiday: false },
+      friday: { open: "", close: "", holiday: false },
+      saturday: { open: "", close: "", holiday: false },
+      sunday: { open: "", close: "", holiday: false },
     },
     constraints: {
-      parking: false, // 주차 가능 여부
+      park: false, // 주차 가능 여부
       wifi: false, // 와이파이 가능 여부
       camera: false, // 촬영 가능 여부
       pet: false, // 반려동물 가능 여부
       kids: false, // 유아동반 가능 여부
-      adult: false, // 미성년자 가능 여부
+      grade: false, // 미성년자 가능 여부
       food: false, // 음식물 반입 가능 여부
     },
-    company: {
-      name: "", // 기업명
-      businessNumber: "", // 사업자 등록번호
-      contact: "", // 기업 전화번호
-      address: "", // 기업 주소
-      condition: "", // 유효 기간/이용 조건
-    },
   });
-
+  
   const changeInformation = (e) => {
     const { name, type } = e.target;
     let { value } = e.target;
-
     const keys = name.split(".");
+    if (name.includes("open") || name.includes("close")) {
+    }
     if (name === "fee") {
       if (isNaN(value.replaceAll(",", ""))) {
         alert("숫자만 입력 가능합니다.");
@@ -90,115 +86,152 @@ export default function ExhibitionRegistPage() {
       });
     }
   };
+ 
   useEffect(() => {
     console.log(information);
   }, [information]);
 
-  const navigate = useNavigate();
-  const handleNavigate = () => {
-    navigate("/event-regist");
+  const registerExhibition = () => {
+    console.log(information);
+    // information data를 형식에 맞게 formData로 변환해서 요청 보내야됨. (multipart/formData);
+    // axios.post('/api/exhibition', information)
+    //     .then((res) => {
+    //         console.log(res);
+    //         if(res.status === 201) {
+    //             alert("이벤트 등록이 완료되었습니다.");
+    //             setStep((prevStep) => prevStep + 1);
+    //         })
+    //     .catch((err) => {
+    //         console.error(err);
+    //     });
+  };
+
+  // const handleNavigate = () => {
+  //   navigate('/event-regist');
+  // }
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // 기본 폼 제출 방지
+    console.log("데이터", data);
+
+    const formData = new FormData();
+    formData.append("userId", data.userId);
+    if (data.image || data.poster) {
+      formData.append("image", data.image);
+      formData.append("poster", data.poster);
+    }
+    try {
+      const res = await ExhibitionAPI.regist(formData);
+      if (res.status === 201) {
+        alert("등록 성공");
+      }
+    } catch (error) {
+      alert("등록 실패");
+      console.log(error.message);
+    }
   };
   return (
     <>
       <ExhibitionHeader step={step} />
       {/* 맨 위 박스  */}
-      <div className="flex flex-col w-5/6 mx-auto gap-5 mt-16">
-        {step === 1 && (
-          <ExStep1
-            information={information}
-            changeInformation={changeInformation}
-          />
-        )}
-        {step === 2 && (
-          <ExStep2
-            information={information}
-            changeInformation={changeInformation}
-          />
-        )}
-        {step === 3 && <div>Step 3</div>}
-        {step === 4 && (
-          <EventRegister
-            information={information}
-            changeInformation={changeInformation}
-          />
-        )}
-
-        <pre className="bg-gray-100 p-4 rounded-lg">
-          {JSON.stringify(information, null, 2)}
-        </pre>
-
-        {/* 하단 단계별 이전, 다음 버튼 */}
-        <div>
-          <hr className="w-full mt-10" />
-          {step === 1 ? (
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                className="border rounded-lg p-3 mt-10"
-                onClick={() => setStep((step) => step + 1)}
-              >
-                다음
-              </button>
-            </div>
-          ) : step === 2 ? (
-            <div className="flex justify-between items-center">
-              <button
-                type="submit"
-                className="border rounded-lg p-3 mt-10"
-                onClick={() => setStep((step) => step - 1)}
-              >
-                이전
-              </button>
-              <button
-                type="submit"
-                className="border rounded-lg p-3 mt-10"
-                onClick={handleNavigate}
-              >
-                이벤트 등록
-              </button>
-              <button
-                type="submit"
-                className="border rounded-lg p-3 mt-10"
-                onClick={() => setStep((step) => step + 2)}
-              >
-                다음
-              </button>
-            </div>
-          ) : step === 3 ? (
-            <div className="flex justify-between items-center">
-              <button
-                type="submit"
-                className="border rounded-lg p-3 mt-10"
-                onClick={() => setStep((step) => step - 1)}
-              >
-                이전
-              </button>
-              <button
-                type="submit"
-                className="border rounded-lg p-3 mt-10"
-                onClick={() => setStep((step) => step + 2)}
-              >
-                등록
-              </button>
-            </div>
-          ) : (
-            <div className="flex justify-between items-center">
-              <button
-                type="submit"
-                className="border rounded-lg p-3 mt-10"
-                onClick={() => setStep((step) => step - 2)}
-              >
-                이전
-              </button>
-              <button
-                type="submit"
-                className="border rounded-lg p-3 mt-10"
-                onClick={() => setStep((step) => step - 1)}
-              >
-                등록
-              </button>
-            </div>
+      <div>
+        <div className="flex flex-col w-5/6 mx-auto gap-5 mt-16">
+          {step === 1 && (
+            <ExStep1
+              information={information}
+              changeInformation={changeInformation}
+            />
           )}
+          {step === 2 && (
+            <ExStep2
+              information={information}
+              changeInformation={changeInformation}
+            />
+          )}
+          {step === 3 && <div>Step 3</div>}
+          {step === 4 && (
+            <EventRegister
+              information={information}
+              changeInformation={changeInformation}
+            />
+          )}
+
+          <pre className="bg-gray-100 p-4 rounded-lg">
+            {JSON.stringify(information, null, 2)}
+          </pre>
+
+          {/* 하단 단계별 이전, 다음 버튼 */}
+          <div>
+            <hr className="w-full mt-10" />
+            {step === 1 ? (
+              <div className="flex justify-end">
+                <button
+                  type="submit"
+                  className="border rounded-lg p-3 mt-10"
+                  onClick={() => setStep((step) => step + 1)}
+                >
+                  다음
+                </button>
+              </div>
+            ) : step === 2 ? (
+              <div className="flex justify-between items-center">
+                <button
+                  type="submit"
+                  className="border rounded-lg p-3 mt-10"
+                  onClick={() => setStep((step) => step - 1)}
+                >
+                  이전
+                </button>
+                <button
+                  type="submit"
+                  className="border rounded-lg p-3 mt-10"
+                  onClick={() => setStep((step) => step + 2)}
+                >
+                  이벤트 등록
+                </button>
+                <button
+                  type="submit"
+                  className="border rounded-lg p-3 mt-10"
+                  onClick={() => setStep((step) => step + 2)}
+                >
+                  다음
+                </button>
+              </div>
+            ) : step === 3 ? (
+              <div className="flex justify-between items-center">
+                <button
+                  type="submit"
+                  className="border rounded-lg p-3 mt-10"
+                  onClick={() => setStep((step) => step - 1)}
+                >
+                  이전
+                </button>
+                <button
+                  type="submit"
+                  className="border rounded-lg p-3 mt-10"
+                  onClick={handleSubmit}
+                >
+                  등록
+                </button>
+              </div>
+            ) : (
+              <div className="flex justify-between items-center">
+                <button
+                  type="submit"
+                  className="border rounded-lg p-3 mt-10"
+                  onClick={() => setStep((step) => step - 2)}
+                >
+                  이전
+                </button>
+                <button
+                  type="submit"
+                  className="border rounded-lg p-3 mt-10"
+                  onClick={() => setStep((step) => step - 1)}
+                >
+                  등록
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
