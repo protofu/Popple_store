76f44d6d88580c3c44913749bf3e1be371c023fc
protofu <sungjae0512@gmail.com>
2024-10-27 00:53:36 +0900

    feat : modal reservation

--- Front-end/popple/src/components/exhibition/Reservation.jsx
+++ Front-end/popple/src/components/exhibition/Reservation.jsx
@@ -15,11 +15,11 @@ export default function Reservation({ reservation, exhi }) {
         <div className="w-full">
           <div className={oneLineStyle}>
             <p className="inline">팝업/전시 명</p>
-            <input type="text" value={exhi.exhibitionName} className={inputStyle} />
+            <input type="text" value={exhi.exhibitionName} className={inputStyle} disabled />
           </div>
           <div className={oneLineStyle}>
             <p className="inline">예약 날짜</p>
-            <input type="text" value={reservationTime} className={inputStyle} />
+            <input type="text" value={reservationTime} className={inputStyle} disabled />
           </div>
         </div>
         {/* 예약자 정보 */}
@@ -27,18 +27,24 @@ export default function Reservation({ reservation, exhi }) {
         <div className="w-full">
           <div className={oneLineStyle}>
             <p className="inline">예약자 성함</p>
-            <input value={exhi.exhibitionName} className={inputStyle}/>
+            <input value={exhi.exhibitionName} className={inputStyle} disabled/>
           </div>
           <div className={oneLineStyle}>
             <p className="inline">이메일</p>
-            <input value={reservationTime} className={inputStyle} />
+            <input value={reservationTime} className={inputStyle} disabled />
           </div>
           <div className={oneLineStyle}>
             <p className="inline">전화번호</p>
-            <input value={reservationTime} className={inputStyle} />
+            <input value={reservationTime} className={inputStyle} disabled />
           </div>
         </div>
         <h1 className="font-bold">입력하신 전화번호로 예약 정보가 발송됩니다.</h1>
+        <div className="flex items-center">
+          <input type="checkbox" className="mr-2" />
+          <p className="text-center text-[0.75rem]">
+            여기는 제 3자의 뭐시기 저시기 정보는 뭐시기 저시기 3년의 뭐시기 저시기 뭐라 했던거 같은데 무튼 그런 내용의 동의 받던가 말던가 하는 내용이 있는 자리
+          </p>
+        </div>
         <div className="bg-popple-light rounded-lg mx-2 my-3 py-2 shadow-xl cursor-pointer w-[200px]">
           <p className="text-center text-white text-[1rem]">예약확정</p>
         </div>
