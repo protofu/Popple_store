b3058a3d7a7dac5fabd4f22464ac8b5282d4b05b
whgms159 <whgms159@naver.com>
2024-10-31 17:07:21 +0900

    feat : required alert , alert util

---
+++ Front-end/popple/src/utils/PoppleAlert.jsx
@@ -0,0 +1,14 @@
+import Swal from 'sweetalert2';
+
+
+export const poppleAlert = {
+  alert : (title, text) => Swal.fire({
+    position: "top-end",
+    timer: 1500,
+    timerProgressBar: true,
+    width: 300,
+    height: 100,
+    title,
+    text
+  })
+};
