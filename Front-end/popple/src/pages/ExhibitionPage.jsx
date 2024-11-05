e58c39088f9655722765a1b08879654ae5b9c036
hoohoha <jae7729@gmail.com>
2024-11-05 17:27:12 +0900

    fix : EventCard를 EnventCartV2로 수정

--- Front-end/popple/src/pages/ExhibitionPage.jsx
+++ Front-end/popple/src/pages/ExhibitionPage.jsx
@@ -11,6 +11,7 @@ import { exhibitionAPI } from "../api/services/Exhibition";
 import { eventAPI } from "../api/services/Event";
 import EventDetailModal from "../components/event/EventDetailModal";
 import NoEventList from "../components/event/NoEventList";
+import EventCardV2 from "../components/exhibition/EventCardV2";
 
 
 
@@ -130,10 +131,10 @@ export default function ExhibitionPage() {
         <img src={eventIcon} alt="이벤트 아이콘" className={titleImgStyle} />
         <h1 className={textStyle}>EVENT</h1>
       </div>
-      <div className="flex flex-wrap justify-center gap-4">
+      <div className="flex flex-wrap gap-4 mx-4">
         {eventList?.length > 0 ?
           eventList.slice(0, 8).map((item, index) => (
-            <EventCard key={index} slogun={item.eventName} title={item.summary} duration={dateToString(item.startAt) + " - " + dateToString(item.endAt)} img={`${eventPosterURL}${item.image}`} onOpen={() => openEventModal(item.id)} id={item.id} />
+            <EventCardV2 key={index} slogun={item.eventName} title={item.summary} duration={dateToString(item.startAt) + " - " + dateToString(item.endAt)} img={`${eventPosterURL}${item.image}`} onOpen={() => openEventModal(item.id)} id={item.id} />
           )) :
           <NoEventList text={"이벤트"}/>
         }
