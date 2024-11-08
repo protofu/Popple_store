1c0841072e539a7220d417c65abff49df894d0ee
whgms159 <whgms159@naver.com>
2024-11-08 12:17:56 +0900

    fix : page btn

--- Front-end/popple/src/pages/ExhibitionPage.jsx
+++ Front-end/popple/src/pages/ExhibitionPage.jsx
@@ -184,7 +184,7 @@ export default function ExhibitionPage() {
             <button
               key={index}
               className={`
-                flex items-center justify-center px-4 h-10 leading-tight text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white rounded-lg m-2"
+                flex items-center rounded-lg m-1 justify-center px-4 h-10 leading-tight text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 ${currentPage === index + 1 && 
                   "bg-popple text-white shadow-lg transform scale-105"
                 }
